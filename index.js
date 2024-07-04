341c7cec431b5ee25721aa95598292a5145461f5
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-07-04 17:56:31 +0300

    Adds modified changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const messages = names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+    return messages;
+}
+function countDown (number){
+    for (let i = number; i >= 0; i--)
+        console.log (i)
+}
